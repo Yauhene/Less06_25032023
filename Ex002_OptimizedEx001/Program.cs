f53bacb259bd8e7c39de8c390a61e4f86cbf4419
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-26 09:21:05 +0300

    Ex002 done

---
+++ Ex002_OptimizedEx001/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = 10;
+int max = 100;
+
+int[] array = new int[n];
+
+
+for (int i = 0; i < n - 1; i++)
+{
+
+    array[i] = Random.Shared.Next(max);
+}
+
+Console.WriteLine($"[{String.Join(',', array)}]");
+
+for (int k = 0; k < n - 1; k++)
+{
+    for (int i = 0; i < n - 1 - k; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+
+    Console.WriteLine($"[{String.Join(',', array)}]");
+}
+
